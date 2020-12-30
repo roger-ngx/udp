23bd295026e71208d1bfe3a04178f816adaf7883
Quang-Phuoc Nguyen <41538596+nqphuoc@users.noreply.github.com>
2020-12-31 04:27:14 +0900

    Update team.js
    first test github

--- components/team.js
+++ components/team.js
@@ -20,7 +20,7 @@ const Team = () => {
                 <Person
                     img='/img/team/phuoc.jpg'
                     name='Phuoc'
-                    role='Manager'
+                    role='Manager test'
                 />
                 <Person
                     img='/img/team/tu.jpg'
