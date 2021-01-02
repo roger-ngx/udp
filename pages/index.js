acb5af0dc5054d6368a278ce04f6ac3ffd421b4b
nqphuoc <nqphuoc@gmail.com>
2021-01-02 17:00:11 +0900

    change title

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>UDP</title>
+        <title>UDP Sol</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
