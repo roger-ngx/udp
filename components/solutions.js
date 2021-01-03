889971bbb57ee905723ed3ef8f38ae710be01faa
thanh nguyen <thanh.uou@gmail.com>
2021-01-03 10:33:18 +0900

    change a products section layout

--- components/solutions.js
+++ components/solutions.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Solution = ({img, title, content, style}) => {
 
     return(
-        <div style={{...style, flex: 1, display: 'flex', flexDirection: 'column', marginTop: 48}}>
-            <img style={{width: '20%', margin: 'auto auto'}} src={img} />
-            <h3>{title}</h3>
-            <p>{content}</p>
+        <div style={{...style, flex: 1, display: 'flex', flexDirection: 'row', marginTop: 48}}>
+            <img style={{width: 90, height: 90, marginRight: 24}} src={img} />
+            <div style={{display: 'flex', flexDirection: 'column', textAlign: 'left'}}>
+                <h3 style={{marginTop: 0, marginBottom: 8, color: '#333'}}>{title}</h3>
+                <p style={{marginTop: 0}}>{content}</p>
+            </div>
         </div>
     )
 }
@@ -16,30 +18,33 @@ const Solutions = () => {
         <>
             <div className='grid' style={{paddingBottom: 0}}>
                 <Solution
+                    style={{marginRight: 24}}
                     img='/img/services/AutoAnnotation_icon.png'
                     title='Automatic Annotation'
                     content='Automatically annotate text data based on predefined categories and entities. '
                 />
                 <Solution
-                    style={{padding: '0 40px'}}
                     img='/img/services/classification_icon.png'
                     title='Content Classification'
                     content='Classify free-text documents by predefined categories to help organize content.'
                 />
+            </div>
+            <div className='grid' style={{paddingBottom: 0}}>
                 <Solution
+                    style={{marginRight: 24}}
                     img='/img/services/InformationExtraction.jpg'
                     title='Entity Extraction'
                     content='Recognize and extract meaningful entities, such as a person, an organization, or location from any text descriptions.'
                 />
-            </div>
-            <div className='grid' style={{paddingTop: 0}}>
                 <Solution
                     img='/img/services/MachineTranslation.jpg'
                     title='Machine Translation'
                     content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.'
                 />
+            </div>
+            <div className='grid' style={{paddingTop: 0}}>
                 <Solution
-                    style={{padding: '0 40px'}}
+                    style={{marginRight: 24}}
                     img='/img/services/Paperless.jpg'
                     title='Paperless'
                     content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.'
